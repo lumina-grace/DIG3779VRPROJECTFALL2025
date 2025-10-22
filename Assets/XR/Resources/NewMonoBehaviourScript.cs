21ea266f8f4c706f43ec08a1346395f6cfb7a38f
lumina-grace <user@FIH-COM23a105.fsu.edu>
2025-10-22 15:35:31 -0400

    bandaids on bug B

---
+++ Assets/XR/Resources/NewMonoBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+// change namespace to your own namespace
+namespace GameUI
+{
+
+    [DefaultExecutionOrder(-1000)]
+    public class DisableMeshOcclusion : MonoBehaviour
+    {
+        // Leave this true to disable the occlusion mesh, only set it false if you want to re‑enable it later at runtime
+        public bool disableOcclusionMesh = true;
+
+        void Awake()
+        {
+            ApplySetting();
+        }
+
+        private void ApplySetting()
+        {
+            if (XRSettings.enabled && disableOcclusionMesh)
+            {
+                XRSettings.useOcclusionMesh = false;
+            }
+        }
+    }
+}
